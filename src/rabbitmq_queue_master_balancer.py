81a1039a21490a3d0c2c710d830eb34ad28b3de8
Sofrony Pavel <pavel@sofrony.ru>
2019-07-30 07:19:46 +0300

    black formater

--- src/rabbitmq_queue_master_balancer.py
+++ src/rabbitmq_queue_master_balancer.py
@@ -10,77 +10,73 @@ from prettylog import basic_config, LogFormat
 from pyrabbit2.api import Client
 from pyrabbit2.http import NetworkError
 
-parser = ArgumentParser(auto_env_var_prefix='RQMB_')
+parser = ArgumentParser(auto_env_var_prefix="RQMB_")
+
+parser.add_argument("--api-url", type=str, default="localhost:15672")
+
+parser.add_argument("--user", type=str, default="guest")
+
+parser.add_argument("--password", type=str, default="guest")
 
 parser.add_argument(
-    '--api-url',
-    type=str,
-    default='localhost:15672'
-)
-
-parser.add_argument(
-    '--user',
-    type=str,
-    default='guest'
-)
-
-parser.add_argument(
-    '--password',
-    type=str,
-    default='guest'
-)
-
-parser.add_argument(
-    '-L', '--log-level', help='Log level',
-    default='info',
+    "-L",
+    "--log-level",
+    help="Log level",
+    default="info",
     choices=(
-        'critical', 'fatal', 'error', 'warning',
-        'warn', 'info', 'debug', 'notset'
+        "critical",
+        "fatal",
+        "error",
+        "warning",
+        "warn",
+        "info",
+        "debug",
+        "notset",
     ),
 )
 
 parser.add_argument(
-    '--log-format', choices=LogFormat.choices(), default=LogFormat.stream
+    "--log-format", choices=LogFormat.choices(), default=LogFormat.stream
 )
 
 parser.add_argument(
-    '--policy-name',
+    "--policy-name",
     type=str,
-    default='queue_master_balancer',
-    help='Policy name for relocate queues'
+    default="queue_master_balancer",
+    help="Policy name for relocate queues",
 )
 
 parser.add_argument(
-    '--dry-run',
+    "--dry-run",
     action="store_true",
     help=(
-        'Show how many queues will move from one node (with a maximum queues)'
-        'to another (with a minimum queues)'
-    )
+        "Show how many queues will move from one node (with a maximum queues)"
+        "to another (with a minimum queues)"
+    ),
 )
 
 parser.add_argument(
-    '--queue-delta',
+    "--queue-delta",
     type=int,
     default=3,
     help=(
-        'Reasonable delta of queues between max and min numbers of queues'
-        'on node when script do nothing'
-    )
+        "Reasonable delta of queues between max and min numbers of queues"
+        "on node when script do nothing"
+    ),
 )
 
 parser.add_argument(
-    '--sleep-time',
+    "--sleep-time",
     type=int,
     default=20,
-    help='Seconds for sleep between queues balancing'
+    help="Seconds for sleep between queues balancing",
 )
 
 parser.add_argument(
-    '--sleep-time-action',
+    "--sleep-time-action",
     type=int,
     default=3,
-    help='Seconds for sleep between RabbitMQ API requests sending'
+    help="Seconds for sleep between RabbitMQ API requests sending",
 )
 
 arguments = parser.parse_args()
@@ -89,12 +85,12 @@ log = logging.getLogger()
 
 
 def wait_for_client(client) -> bool:
-    '''
+    """
     :param client:
     :return: bool
-    '''
+    """
 
-    log.debug('RabbitMQ http not ready, waiting...')
+    log.debug("RabbitMQ http not ready, waiting...")
     try:
         return client.is_alive()
     except NetworkError:
@@ -103,27 +99,27 @@ def wait_for_client(client) -> bool:
 
 
 def nodes_dict(nodes_info_data, vhost_names: list) -> Dict[str, List]:
-    '''
+    """
     :param client
     :param vhost_names list of vhosts
     :return: dict: Key is a name of rabbit node, Value is empty list
-    '''
+    """
     temp_dict = dict.fromkeys((vhost for vhost in vhost_names))
 
     nodes_dictionary = dict.fromkeys(
-        (node['name'] for node in nodes_info_data), temp_dict
+        (node["name"] for node in nodes_info_data), temp_dict
     )
     return nodes_dictionary
 
 
 def master_nodes_queues(
-        nodes_dict: dict, vhost_names: list
+    nodes_dict: dict, vhost_names: list
 ) -> Dict[str, Dict[str, List]]:
-    '''
+    """
     :param queues_data: list of dicts with all queues info
     :param vhost_names list of vhosts
     :return: dict {node_name: {vhost1: list_queues, vhost2: list_queues}
-    '''
+    """
 
     master_nodes_queues_dict = {}
 
@@ -136,8 +132,8 @@ def master_nodes_queues(
             list_queues = []
 
             for queue in queues_data:
-                if node == queue['node'] and queue['vhost'] == vhost:
-                    list_queues.append(queue['name'])
+                if node == queue["node"] and queue["vhost"] == vhost:
+                    list_queues.append(queue["name"])
 
             vhost_dict[vhost] = list_queues
             master_nodes_queues_dict[node] = vhost_dict
@@ -146,10 +142,10 @@ def master_nodes_queues(
 
 
 def calculate_queues(master_nodes_queues_dict: dict) -> Dict[str, int]:
-    '''
+    """
     :param master_nodes_queues_dict: dict
     :return: dict {node1: number_queues, node2: number_queues,}
-    '''
+    """
 
     calculated_dict = {}
 
@@ -161,10 +157,10 @@ def calculate_queues(master_nodes_queues_dict: dict) -> Dict[str, int]:
 
 
 def calculate_vhost(max_master_dict: dict) -> Dict[str, int]:
-    '''
+    """
     :param max_master_dict:
     :return: dict {vhost, number_queues}
-    '''
+    """
     calculated_dict = {}
 
     for vhost, queues in max_master_dict.items():
@@ -174,121 +170,108 @@ def calculate_vhost(max_master_dict: dict) -> Dict[str, int]:
     return calculated_dict
 
 
-def is_relocate(max_queues: int,
-                min_queues: int,
-                queue_delta: int
-                ) -> bool:
-    '''
+def is_relocate(max_queues: int, min_queues: int, queue_delta: int) -> bool:
+    """
     :param min_queues: int
     :param max_queues: int
     :return: bool
-    '''
+    """
     return max_queues - min_queues > queue_delta
 
 
-def relocate_policy(queue_for_relocate: str,
-                    max_queues_vhost: str,
-                    min_queues_node: str,
-                    policy_name: str
-                    ):
+def relocate_policy(
+    queue_for_relocate: str,
+    max_queues_vhost: str,
+    min_queues_node: str,
+    policy_name: str,
+):
 
-    definition_dict = {'ha-mode': 'nodes',
-                       "ha-params": min_queues_node.split(' ')
-                       }
-    dict_params = {'pattern': queue_for_relocate,
-                   'definition': definition_dict,
-                   'priority': 999,
-                   "apply-to": "queues"
-                   }
-    log.debug('Policy body dict is %r', dict_params)
+    definition_dict = {
+        "ha-mode": "nodes",
+        "ha-params": min_queues_node.split(" "),
+    }
+    dict_params = {
+        "pattern": queue_for_relocate,
+        "definition": definition_dict,
+        "priority": 999,
+        "apply-to": "queues",
+    }
+    log.debug("Policy body dict is %r", dict_params)
 
     client.create_policy(
-        vhost=max_queues_vhost,
-        policy_name=policy_name,
-        **dict_params
+        vhost=max_queues_vhost, policy_name=policy_name, **dict_params
     )
 
 
 def is_queue_running(client, vhost: str, queue: str) -> bool:
     state = None
-    while state != 'running':
+    while state != "running":
         try:
-            state = client.get_queue(vhost, queue)['state']
-            log.debug('Queue %r has state %r', queue, state)
-            if state != 'running':
+            state = client.get_queue(vhost, queue)["state"]
+            log.debug("Queue %r has state %r", queue, state)
+            if state != "running":
                 time.sleep(1)
             else:
                 return True
         except KeyError:
-            log.exception('RabbitMQ API not ready to answer')
+            log.exception("RabbitMQ API not ready to answer")
 
 
-def relocate_queue(client,
-                   max_master_dict: dict,
-                   max_queues_vhost: str,
-                   min_queues_node: str,
-                   sleep_time: int
-                   ):
+def relocate_queue(
+    client,
+    max_master_dict: dict,
+    max_queues_vhost: str,
+    min_queues_node: str,
+    sleep_time: int,
+):
 
     queue_for_relocate = max_master_dict[max_queues_vhost][0]
-    log.debug('Queue for relocate is %r', queue_for_relocate)
-    log.debug('Creating relocate policy')
+    log.debug("Queue for relocate is %r", queue_for_relocate)
+    log.debug("Creating relocate policy")
     relocate_policy(
         queue_for_relocate,
         max_queues_vhost,
         min_queues_node,
-        arguments.policy_name
+        arguments.policy_name,
     )
     time.sleep(sleep_time)
-    client.queue_action(
-        max_queues_vhost,
-        queue_for_relocate,
-        action='sync'
-    )
+    client.queue_action(max_queues_vhost, queue_for_relocate, action="sync")
     time.sleep(sleep_time)
     is_queue_running(client, max_queues_vhost, queue_for_relocate)
-    client.queue_action(
-        max_queues_vhost,
-        queue_for_relocate,
-        action='sync'
-    )
+    client.queue_action(max_queues_vhost, queue_for_relocate, action="sync")
     time.sleep(sleep_time)
     client.delete_policy(max_queues_vhost, arguments.policy_name)
     time.sleep(sleep_time)
-    client.queue_action(
-        max_queues_vhost,
-        queue_for_relocate,
-        action='sync'
-    )
+    client.queue_action(max_queues_vhost, queue_for_relocate, action="sync")
     time.sleep(sleep_time)
     is_queue_running(client, max_queues_vhost, queue_for_relocate)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     basic_config(
         level=arguments.log_level.upper(),
         buffered=False,
         log_format=arguments.log_format,
-        date_format=True
+        date_format=True,
     )
 
     client = Client(arguments.api_url, arguments.user, arguments.password)
 
     wait_for_client(client)
-    log.debug('RabbitMQ alive')
+    log.debug("RabbitMQ alive")
 
     while True:
         nodes_info_data = client.get_nodes()
 
-        log.debug('Nodes info: %r', nodes_info_data)
+        log.debug("Nodes info: %r", nodes_info_data)
 
         vhost_names = client.get_vhost_names()
-        log.debug('Vhost names: %r', vhost_names)
+        log.debug("Vhost names: %r", vhost_names)
 
         master_nodes_queues_dict = master_nodes_queues(
             nodes_dict(nodes_info_data, vhost_names), vhost_names
         )
-        log.debug('Master nodes info: %r', master_nodes_queues_dict)
+        log.debug("Master nodes info: %r", master_nodes_queues_dict)
 
         calculated_queues = calculate_queues(master_nodes_queues_dict)
 
@@ -296,9 +279,9 @@ if __name__ == '__main__':
         max_queues_node = max(calculated_queues, key=calculated_queues.get)
 
         min_queues = calculated_queues.get(min_queues_node)
-        log.debug('Min number of queues is %r', min_queues)
+        log.debug("Min number of queues is %r", min_queues)
         max_queues = calculated_queues.get(max_queues_node)
-        log.debug('Max number of queues is %r', max_queues)
+        log.debug("Max number of queues is %r", max_queues)
 
         max_master_dict = master_nodes_queues_dict[max_queues_node]
         calculated_vhost = calculate_vhost(max_master_dict)
@@ -307,11 +290,7 @@ if __name__ == '__main__':
         if arguments.dry_run:
             log.info("It's a dry run. Calculate queues...")
             for node, queue_number in calculated_queues.items():
-                log.info(
-                    'Node %r is a master of %r queues',
-                    node,
-                    queue_number
-                )
+                log.info("Node %r is a master of %r queues", node, queue_number)
             sys.exit(0)
 
         if is_relocate(max_queues, min_queues, arguments.queue_delta):
@@ -323,9 +302,8 @@ if __name__ == '__main__':
                 sleep_time=arguments.sleep_time_action,
             )
             log.debug(
-                'Queue balancer sleeping for %r seconds',
-                arguments.sleep_time
+                "Queue balancer sleeping for %r seconds", arguments.sleep_time
             )
             time.sleep(arguments.sleep_time)
         else:
-            log.info('Nothing to do')
+            log.info("Nothing to do")
